1ca673fd0ac3c5124b8390499f953d2bb3793c24
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2025-04-06 18:20:57 +0530

    Add Foooter component for footer section with copyright information

---
+++ src/components/custom/Foooter.tsx
@@ -0,0 +1,14 @@
+function Foooter() {
+  return (
+    <footer className="bg-white  ">
+      <div className="text-center text-2xl font-bold text-white bg-red-700 py-4 mt-10 bottom-0">
+        <p>
+          © 2024 All Rights Reserved. Design by
+          <span className="font-bold"> Citrio Solutions, Bangalore</span>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Foooter;
