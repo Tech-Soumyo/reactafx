a744b1e14ab5ab39fa44dca0c8f1d0925e9dcc0d
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2025-04-06 11:55:53 +0530

    Refactor App component to simplify routing and remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Animation2D } from "./components/custom/2DAnimation";
-import { ContactUs } from "./components/custom/Contact";
-import { Expertise } from "./components/custom/Expertise";
-import { Explainer3D } from "./components/custom/Explainer3D";
-import Headers from "./components/custom/Headers";
-import { OneCarousel } from "./components/custom/OneCorousel";
-import { SkateShopSection } from "./components/custom/SkateShop";
-import { TestimonialCarousel } from "./components/custom/Testimoinals";
-
-import VideoSection from "./components/custom/YtSection";
-import CanvasCursor from "./components/ui/CanvasCursor";
-import FluidCursor from "./components/ui/FluidCursor";
-import { SplashCursor } from "./components/ui/splash-cursor";
+import { MainPage } from "./components/custom/mainPage";
+import ReadPage from "./components/custom/ReadPage";
 
 function App() {
   return (
     <>
       {/* <SplashCursor /> */}
-      {/* <Routes>
-        <Route path="/" element={<Headers />}></Route>
-      </Routes> */}
-      <CanvasCursor />
-      {/* <FluidCursor /> */}
-      <Headers />
-      <OneCarousel />
-      <Expertise />
-      <VideoSection />
-      <SkateShopSection />
-      <Animation2D />
-      <Explainer3D />
-      <TestimonialCarousel />
-      <ContactUs />
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/read" element={<ReadPage />}></Route>
+      </Routes>
     </>
   );
 }
